dfed7e280b5b89bd64ae3a635dc374b8c5a6df48
daviwesley <davi.wesley@icloud.com>
2015-09-18 14:43:16 -0300

    adicionado setters e getters

--- src/robot/ninja/davi/Robot.java
+++ src/robot/ninja/davi/Robot.java
@@ -1,5 +1,42 @@
 package robot.ninja.davi;
 
 public class Robot {
+	 // as variaveis dentro da classe sao chamadas de variaveis de instancia
+    private int nivelBateria;
+    public int getNivelBateria() {
+		return nivelBateria;
+	}
+	public void setNivelBateria(int nivelBateria) {
+		this.nivelBateria = nivelBateria;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public char getSexo() {
+		return sexo;
+	}
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+	public int getCoordX() {
+		return coordX;
+	}
+	public void setCoordX(int coordX) {
+		this.coordX = coordX;
+	}
+	public int getCoordY() {
+		return coordY;
+	}
+	public void setCoordY(int coordY) {
+		this.coordY = coordY;
+	}
+	private String nome;
+    private char sexo;
+    private int coordX;
+    private int coordY;
+    
 
 }
--- src/robot/ninja/davi/TestaRobot.java
+++ src/robot/ninja/davi/TestaRobot.java
@@ -1,5 +1,10 @@
 package robot.ninja.davi;
 
 public class TestaRobot {
+	public static void main (String[] Args){ // metodo por onde a execucao vai começar
+        //criando objeto - declarar variavel que faca referencia ao objeto robot
+        Robot r2d2; //r2d2 é uma variavel de referencia
+           
+    }
 
 }
