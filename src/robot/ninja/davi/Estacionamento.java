de7f79f4212e46a5591593e18ab9fc29fcc5335b
daviwesley <davi.wesley@icloud.com>
2015-09-25 15:47:45 -0300

    adicionado método entrada()

--- src/robot/ninja/davi/Estacionamento.java
+++ src/robot/ninja/davi/Estacionamento.java
@@ -1,19 +1,20 @@
 package robot.ninja.davi;
 
 public class Estacionamento {
-	int vagas[];
-	int valorPago;
-	public int[] getVagas() {
-		return vagas;
-	}
-	public void setVagas(int[] vagas) {
-		this.vagas = vagas;
-	}
-	public int getValorPago() {
-		return valorPago;
-	}
-	public void setValorPago(int valorPago) {
-		this.valorPago = valorPago;
+	private Carro[] vagas = new Carro[100];
+	private static int totalVagas = 100;
+	private static float lucroDia= 0;
+	
+	public void entrada (Carro carro){
+		if (totalVagas >0){
+			for (int i = 0; i <vagas.length; i++){
+				if (vagas[i]!=null){
+					vagas[i]= carro;
+					totalVagas--;
+					break;
+				}
+			}
+		}
 	}
 
 }
