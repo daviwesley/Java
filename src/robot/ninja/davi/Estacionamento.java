3934839842ec3cc343c056b349f8ba56b7201498
daviwesley <davi.wesley@icloud.com>
2015-09-25 15:32:43 -0300

    Métodos adicionados

--- src/robot/ninja/davi/Estacionamento.java
+++ src/robot/ninja/davi/Estacionamento.java
@@ -2,6 +2,18 @@ package robot.ninja.davi;
 
 public class Estacionamento {
 	int vagas[];
-	int valorPago[];
+	int valorPago;
+	public int[] getVagas() {
+		return vagas;
+	}
+	public void setVagas(int[] vagas) {
+		this.vagas = vagas;
+	}
+	public int getValorPago() {
+		return valorPago;
+	}
+	public void setValorPago(int valorPago) {
+		this.valorPago = valorPago;
+	}
 
 }
